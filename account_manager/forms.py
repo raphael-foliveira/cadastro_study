19a223c329f95fe80485e4e468801044555e8200
raphael <raphael.foliveira1@gmail.com>
2022-06-16 11:04:22 -0300

    formatting

--- account_manager/forms.py
+++ account_manager/forms.py
@@ -4,5 +4,5 @@ from django import forms
 class CreateAccountForm(forms.Form):
     account_name = forms.CharField(max_length=100, label="Name: ")
     account_password = forms.CharField(
-        max_length=100, label="Password: ", widget=forms.PasswordInput())
+        max_length = 100, label="Password: ", widget=forms.PasswordInput())
     account_data = forms.CharField(max_length=200, label="Content: ")
