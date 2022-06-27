bd4da56f673efbfd3220bf647a6ea930ae8f149d
raphael <raphael.foliveira1@gmail.com>
2022-06-27 18:23:36 -0300

    add-static-dir

--- account_manager/forms.py
+++ account_manager/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class CreateAccountForm(forms.Form):
-    account_name = forms.CharField(max_length=100, label="Name: ")
+    account_name = forms.CharField(max_length=100, label="Name")
     account_password = forms.CharField(
-        max_length = 100, label="Password: ", widget=forms.PasswordInput())
-    account_data = forms.CharField(max_length=200, label="Content: ")
+        max_length = 100, label="Password", widget=forms.PasswordInput())
+    account_data = forms.CharField(max_length=200, label="Content")
